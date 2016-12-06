9bf60f094c595d2dcfb98ed6342710524806b8ae
Emiliano Daddario <josieusa@users.noreply.github.com>
2016-12-06 08:34:31 +0100

    Support looping syntax with key-value pairs

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 module.exports.makeObjectsIterable = () => {
 	Object.prototype[Symbol.iterator] = function* () {
 		for (const e of Object.entries(this)) {
-			yield {
-				k: e[0],
-				v: e[1],
-			};
+			yield Object.assign(e, {
+			  k: e[0],
+			  key: e[0],
+			  v: e[1],
+			  val: e[1],
+			  value: e[1],
+			});
 		}
 	};
 };
